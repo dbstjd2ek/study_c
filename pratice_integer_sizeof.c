d836b6b1cb31a9a3b74c8ebb955b6efdda3e7681
dbstjd2ek <dbstjd2ek@gmail.com>
2026-01-23 01:18:38 +0900

    pratice_integer_max.c

--- pratice_integer_sizeof.c
+++ pratice_integer_sizeof.c
@@ -4,7 +4,7 @@ int main()
     unsigned short num1;
     unsigned long long num2;
 
-    printf("%d\n",sizeof(num1), sizeof(num2) + sizeof(int);
+    printf("%d\n",sizeof(num1) + sizeof(num2) + sizeof(int);
 
     return 0;
 }
