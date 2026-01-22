4eb15f996c96370f43c4a4d128fa5e78b23039b0
dbstjd2ek <dbstjd2ek@gmail.com>
2026-01-23 01:03:54 +0900

    pratice_integer_overflow.c

---
+++ pratice_integer_overflow.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    unsigned char num1 = 256;
+    unsigned short num2 = 65536;
+    long long num3 = 9223372036854775808;
+
+    printf("%u %u %llong\n", num1, num2 ,num3);
+    return 0;
+}
