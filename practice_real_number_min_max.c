02fbfb8f4ab729fb736ff7b21148f37b440cbb52
dbstjd2ek <dbstjd2ek@gmail.com>
2026-01-27 23:21:06 +0900

    practice_real_number_min_max.c

---
+++ practice_real_number_min_max.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<float.h>
+int main()
+{
+float num1 = FLT_MAX;
+
+double num2 = DBL_MIN;
+
+long double num3 = LDBL_MIN;
+
+printf("%.2f\n", num1);
+
+printf("%e\n", num2);
+
+printf("%Le\n", num3)
+return 0;
+}
